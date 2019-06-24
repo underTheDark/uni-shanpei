a36f73469f2f1201b01cd12e318c49245050f80a
joe <980846821@qq.com>
2019-06-24 18:43:56 +0800

    6/24

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,25 +1,25 @@
 <template>
 	<view class="index">
 		<view class="index-head">
-			<image src="../../static/image/index/menu.png"></image>
+			<image src="../../static/image/index/menu.png" @click="menu" />
 			<view class="address-select">
-				<text>dadadaa</text>
-				<image src="../../static/image/index/xiala.png"></image>
+				<text>大城小爱小区</text>
+				<image src="../../static/image/index/xiala.png" />
 			</view>
-			<image src="../../static/image/index/saoma.png"></image>
+			<image src="../../static/image/index/saoma.png" />
 		</view>
 		<view class="index-main">
 			<view class="getgoods-type">
 				<text>自提点</text>
 				<text>用户家中</text>
 			</view>
-			<view class="search"> 
-			    <view class="search-left">
+			<view class="search">
+				<view class="search-left">
 					<image src="../../static/image/index/search.png" class="fangdajing-icon"></image>
-				    <input type="text" />
+					<input type="text" />
 					<image src="../../static/image/index/close.png" class="close-icon"></image>
 				</view>
-				<view>搜索</view>
+				<view class="search-right">搜索</view>
 			</view>
 			<view class="user-message">
 				<view class="user-message-list">
@@ -41,72 +41,122 @@
 				</view>
 			</view>
 		</view>
+		<view class="get-type">
+			<text>签收</text>
+			<text>类型</text>
+		</view>
+		<!-- 菜单 -->
+
+		<view class="filter-net" v-show="show">
+			<view class="menu">
+				<view class="user-msg">
+					<image src="../../static/image/index/close.png"></image>
+					<view>
+						<text>姓名</text>
+						<text>电话</text>
+					</view>
+				</view>
+				<view class="user-income">
+					<view class="get-money">
+						<image src="../../static/image/index/month-income.png"></image>
+						<text>本月收入</text>
+						<text>￥100</text>
+
+					</view>
+					<view class="get-money">
+						<image src="../../static/image/index/month-income.png"></image>
+						<text>卡券收入</text>
+						<text>100个</text>
+
+					</view>
+					<view class="get-money">
+						<image src="../../static/image/index/month-income.png"></image>
+						<text>累计收入</text>
+						<text>￥100</text>
+
+					</view>
+					<view class="get-money date-select">
+						<image src="../../static/image/index/month-income.png"></image>
+						<text>历史收入</text>
+						<text>￥100</text>
+						<view class="date-month" @click="dateMonth">
+							<text>{{month}}月</text>
+							<image src="../../static/image/index/right-jiantou.png"></image>
+							<view class="month-select">
+								<picker-view class="month-select-num" v-if="visible" :indicator-style="indicatorStyle"  @change="bindChange">
+									
+									<picker-view-column>
+										<view class="item" v-for="(item,index) in months" :key="index" @click="monthSelect(e)">{{item}}月</view>
+									</picker-view-column>
+									
+								</picker-view>
+							</view>
+						</view>
+					</view>
+					<view class="get-money">
+						<image src="../../static/image/index/month-income.png"></image>
+						<navigator url="/pages/login/login">
+							<text>退出登录</text>
+						</navigator>
+
+
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
-	
+
 </template>
 
 <script>
 	export default {
 		data() {
+			const date = new Date()
+            const years = []
+            const year = date.getFullYear()
+            const months = []
+            const month = date.getMonth() + 1
+            const days = []
+            const day = date.getDate()
+			for (let i = 1990; i <= date.getFullYear(); i++) {
+            years.push(i)
+        }
+        for (let i = 1; i <= 12; i++) {
+            months.push(i)
+        }
+        for (let i = 1; i <= 31; i++) {
+            days.push(i)
+        }
 			return {
-				
+				show: false,
+				months,
+                month,
+				visible:false,
+				indicatorStyle:`color:green`,
+			
 			};
+		},
+		methods: {
+			menu() {
+				this.show = true;
+			},
+		     bindChange: function (e) {
+            	 const val = e.detail.value
+             console.log(this.months[val[0]])
+            this.month = this.months[val[0]]
+            },
+			dateMonth(){
+				this.visible=true;
+			},
+			monthSelect(e){
+				console.log(e)
+			
+          
+			}
 		}
 	}
 </script>
 
 <style>
-	.login{
-		padding:165upx 0 0 60upx;
-		display: flex;
-		flex-direction: column;
-	}
-		
-	.login h1{
-width:222upx;
-height:52upx;
-font-size:52upx;
-color:rgba(51,51,51,1);
-line-height:78upx;
-letter-spacing:3upx;
-border-bottom: 8px solid #00C65D;
-
-	}
-	.login text{
-		width:150upx;
-height:38upx;
-font-size:38upx;
-color:rgba(51,51,51,1);
-line-height:56upx;
-margin:25upx 0 241upx;
-	}
-	.login-form{
-		width:656upx;
-        display: flex;
-		flex-direction: column;
-	}
-	.login-form input{
-		padding-bottom: 43upx;
-        border-bottom:1px solid rgba(232,232,232,1);
-		font-size:29upx;
-	}
-	.login-form input:nth-child(2){
-		margin:96upx 0 245upx;
-	}
-	.login button{
-		width:656upx;
-		height:96upx;
-		line-height: 96upx;
-		background-color: #00C65D;
-		border-radius:96upx;
-		margin-left:-15upx;
-		color:white;
-		font-size: 31upx;
-		letter-spacing: 1px;
-		/* box-shadow: 0px 10upx 20upx  #00D363;
-		-webkit-box-shadow: 0px 10upx 20upx  #00D363;
-		-moz-box-shadow: 0px 10upx 20upx  #00D363;
-		-ms-box-shadow: 0px 10upx 20upx  #00D363;
-		-o-box-shadow: 0px 10upx 20upx  #00D363; */
-	}
+	@import "index.css"
 </style>
---
+++ pages/remark/remark.vue
@@ -0,0 +1,207 @@
+<template>
+	<view class="remark">
+		<view class="remark-head">
+			<view>&lt;</view>
+			<view>备注</view>
+			<view>提交</view>
+		</view>
+		<view class="remark-main">
+			<view>
+				<text>订单编号：</text>
+				<text>22222222</text>
+			</view>
+			<view>
+				<text>收货人：</text>
+				<text>joe</text>
+			</view>
+			
+			<view>
+				<text>电话：</text>
+				<text>2333333333</text>
+			</view>
+			<view>
+				<text>地址：</text>
+				<text>22222222</text>
+			</view>
+			<view>
+				<text>配送件数：</text>
+				<text>2</text>
+			</view>
+			<view>
+				<text>收货时间：</text>
+				<text>22222222</text>
+			</view>
+		</view>
+		<view class="remark-reason">
+			<view>
+				<text>备注：</text>
+				<text>22222222</text>
+			</view>
+		    <image src="../../static/image/remark/xiala-icon.png" />
+		</view>
+		<view class="remark-message">
+			<textarea placeholder="请输入备注信息" maxlength="500"></textarea>
+			<view>30/500</view>
+		</view>
+		<view class="remark-upload">
+			<view>上传照片</view>
+			<view>
+				<image src="../../static/image/remark/camera-icon.png" />
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style>
+	.remark{
+		display:flex;
+		flex-direction: column;
+		padding:0 40upx;
+		flex:1
+	}
+	.remark-head{
+		display:flex;
+		justify-content: space-between;
+		align-items: center;
+		height:100upx;
+		
+	}
+	.remark-head view:nth-child(1){
+		
+font-size: 70upx;
+	}
+	.remark-head view:nth-child(2){
+		font-size:38upx;
+font-family:SourceHanSansCN-Medium;
+font-weight:600;
+color:rgba(51,51,51,1);
+	}
+	.remark-head view:nth-child(3){
+		
+width:115upx;
+height:50upx;
+background:rgba(0,198,93,1);
+box-shadow:0px 2px 8px 0px rgba(0,198,93,0.36);
+border-radius:25px;
+line-height:50upx;
+display:flex;
+justify-content: center;
+font-size: 29upx;
+color:#FFFFFF;
+	}
+	
+ /* 用户信息 */
+ .remark-main{
+	 display: flex;
+	 flex-direction: column;
+	 padding:25upx 0 25upx 25upx;
+	  background: url(../../static/image/index/shugang.png) no-repeat 0 50upx;
+	  background-size: 4upx 42upx;
+ }
+ .remark-main view{
+	 display:flex;
+	 justify-content: flex-start;
+	 align-items: center;
+	 font-size: 29upx;
+	margin:25upx 0; 
+ }
+ .remark-main view text:nth-child(1){
+	 color:#999999
+ }
+ .remark-main view text:nth-child(2){
+	 color:#333333;
+ }
+ /* 货物丢失备注 */
+ .remark-reason {
+	 display:flex;
+	 justify-content: space-between;
+	 align-items: center;
+	 height:125upx;
+	 background: url(../../static/image/index/shugang.png) no-repeat 0 center;
+	  background-size: 4upx 42upx;
+	 padding-left:25upx;
+ }
+ .remark-reason view{
+	 display:flex;
+	 justify-content: flex-start;
+	 align-items: center;
+ }
+ .remark-reason view text:nth-child(1){
+	 color:#999999
+ }
+  .remark-reason view text:nth-child(2){
+ 	 color:#333
+ }
+ .remark-reason image{
+	 width:56upx;
+	 height:56upx;
+ }
+ 
+ .remark-message{
+	 display:flex;
+	 flex-direction: column;
+	  background: url(../../static/image/index/shugang.png) no-repeat 0 48upx;
+	   background-size: 4upx 42upx;
+	
+ }
+ .remark-message textarea{
+	 height:271upx;
+	 background:rgba(246,248,247,1);
+	 font-size: 29upx;
+	 color:#999999;
+	 width:100%;
+	 padding: 48upx 0 0 25upx;
+	 
+ }
+ .remark-message view{
+	 height:63upx;
+	 
+	 font-size: 25upx;
+	 color:#CCCCCC;
+	 display:flex;
+	 justify-content: right;
+	 align-items: center;
+ }
+ 
+ /* 上传照片 */
+ 
+ .remark-upload{
+	 padding:50upx 0 50upx 25upx;
+	 display:flex;
+	 flex-direction: column;
+	 height:333upx;
+	  background: url(../../static/image/index/shugang.png) no-repeat 0 50upx;
+	  background-size: 4upx 42upx;
+ }
+ .remark-upload view:nth-child(1){
+	 
+height:29upx;
+font-size:29upx;
+color:rgba(153,153,153,1);
+letter-spacing:1px;
+ }
+ .remark-upload view:nth-child(2){
+	 width:177upx;
+height:177upx;
+border-radius:2upx;
+border:1px dotted rgba(151,151,151,1);
+margin-top: 29upx;
+display: flex;
+justify-content: center;
+align-items: center;
+ }
+ .remark-upload image{
+	 width:74upx;
+	 height:72upx;
+ }
+</style>
